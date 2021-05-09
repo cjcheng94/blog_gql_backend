f0513182ec950eb38b83afb94dbc35757be43310
Alex Cheng <alexcheng94@126.com>
2021-05-09 17:04:58 +0800

    ✨ implemented update and delete post

--- src/posts/resolvers.ts
+++ src/posts/resolvers.ts
@@ -12,17 +12,35 @@ interface Resolvers extends WithIndexSignature {
 const resolvers: Resolvers = {
   Query: {
     async posts(parent, args, context) {
-      const data = await context.db
-        .collection("posts")
-        .find()
-        .toArray();
-      return data;
+      try {
+        const data = await context.db
+          .collection("posts")
+          .find()
+          .toArray();
+        return data;
+      } catch (err) {
+        console.log("Error, database error");
+        return;
+      }
     },
     async getPostById(parent, args, context) {
       const { _id } = args;
       const objId = new ObjectId(_id);
-      const data = await context.db.collection("posts").findOne({ _id: objId });
-      return data;
+      try {
+        const data = await context.db
+          .collection("posts")
+          .findOne({ _id: objId });
+
+        if (data === null) {
+          console.log("Error, cannot find post");
+          return;
+        }
+
+        return data;
+      } catch (err) {
+        console.log("Error, database error");
+        return;
+      }
     }
   },
   Mutation: {
@@ -30,25 +48,126 @@ const resolvers: Resolvers = {
       const { title, content } = args;
       const { isAuthed, userData } = context;
 
+      // User not logged in
       if (isAuthed) {
-        // create new Post
-        const newPost = {
-          title,
-          content,
-          _id: new ObjectId(),
-          author: userData.userId,
-          date: new Date().toISOString()
-        };
+        console.log("Error, unauthorized");
+        return;
+      }
 
+      // create new Post
+      const newPost = {
+        title,
+        content,
+        _id: new ObjectId(),
+        author: userData.userId,
+        date: new Date().toISOString()
+      };
+
+      try {
         const dbRes = await context.db.collection("posts").insertOne(newPost);
         const newPostData = dbRes.ops[0];
         return newPostData;
+      } catch (err) {
+        console.log("Error, database error");
+        return;
       }
-      // todo: add error
-      console.log("UNAUTHORIZED");
+    },
+    async updatePost(parent, args, context) {
+      const { _id, title, content } = args;
+      const { isAuthed, userData, db } = context;
+
+      // User not logged in
+      if (!isAuthed) {
+        console.log("Error, unauthorized");
+        return;
+      }
+
+      // Find to-be-updated post by id and its owner
+      const objId = new ObjectId(_id);
+      const postToUpdate = await context.db
+        .collection("posts")
+        .findOne({ _id: objId });
+      const postOwner = postToUpdate.author;
+
+      // Not your post
+      if (postOwner !== userData.userId) {
+        console.log("Error, not your post");
+        return;
+      }
+
+      // update post
+      try {
+        const dbRes = await db
+          .collection("posts")
+          .updateOne({ _id: objId }, { $set: { title, content } });
+
+        // Operation error
+        if (dbRes.matchedCount !== 1 || dbRes.modifiedCount !== 1) {
+          console.log("Error, operation error");
+          return;
+        }
+
+        // update success
+        const updatedPost = await context.db
+          .collection("posts")
+          .findOne({ _id: objId });
+
+        // Cannot find such a post
+        if (updatedPost === null) {
+          console.log("Error, cannot find updated post");
+          return;
+        }
+        // All done, return updated post
+        return updatedPost;
+      } catch (err) {
+        console.log("Error, database error");
+        return;
+      }
+    },
+    async deletePost(parent, args, context) {
+      const { _id } = args;
+      const { isAuthed, userData, db } = context;
+
+      // User not logged in
+      if (!isAuthed) {
+        console.log("Error, unauthorized");
+        return;
+      }
+
+      const objId = new ObjectId(_id);
+      const postToDelete = await context.db
+        .collection("posts")
+        .findOne({ _id: objId });
+
+      // Cannot find such a post
+      if (postToDelete === null) {
+        console.log("Error, Cannot find post");
+        return;
+      }
+
+      const postOwner = postToDelete.author;
+
+      // Not your post
+      if (postOwner !== userData.userId) {
+        console.log("Error, not your post");
+        return;
+      }
+
+      // Delete post
+      try {
+        const dbRes = await db.collection("posts").deleteOne({ _id: objId });
+        // Operation error(e.g.cant find post)
+        if (dbRes.deletedCount < 1) {
+          console.log("Error, operation error");
+          return;
+        }
+        // Deleted successfully
+        return postToDelete;
+      } catch (err) {
+        console.log("Error, database error");
+        return;
+      }
     }
-    // async updatePost(parent, args, context) {},
-    // async deletePost(parent, args, context) {}
   }
 };
 
