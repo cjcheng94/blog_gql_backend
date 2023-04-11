37595f1527ce434b4e0ee741e64886acb30d3243
Alex Cheng <alexcheng94@126.com>
2023-04-11 17:32:08 +0800

    ♻️ added custom errors extending from GraphQLError

--- src/errors/notFound.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloError } from "apollo-server";
-
-export default class NotFoundError extends ApolloError {
-  constructor(message: string) {
-    super(message, "NOT_FOUND");
-    Object.defineProperty(this, "name", { value: "NOT_FOUND" });
-  }
-}
